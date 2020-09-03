45c7bc14f248c7e2caf62c622fce3baf97dc3581
Rewindding <Rewindding@outlook.com>
2020-09-03 19:32:39 +0800

    Update stream_reassembler.cc

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -26,11 +26,11 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
     if(eof) last_byte_num=data_end_index;
     if(index-rcv_base>=_capacity) return;//out of window bound;
     else if(data_end_index<next_seq) return;//completely duplicate
-    size_t start_ind=max(index-rcv_base,next_seq-rcv_base);
-    size_t len=min(_capacity-start_ind,data.size());//max length that could be writed
-    //index kills !!!
     size_t zero=0;
-    for(size_t i=start_ind,k=max(zero,next_seq-index);i-start_ind<len;++i,++k){
+    size_t start_index=index;
+    size_t len=min(_capacity+rcv_base-next_seq,data.size());
+    size_t offset=max(zero,next_seq-index);
+    for(size_t i=start_index+offset,k=offset;k<len;++i,++k){
         if(!received[i]){
             window[i]=data[k];
             received[i]=true;
@@ -38,7 +38,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         }
     }
     trans_data();
-   if(rcv_base==last_byte_num+1){
+    if(rcv_base==last_byte_num+1){
        _output.end_input();
    }
 }
