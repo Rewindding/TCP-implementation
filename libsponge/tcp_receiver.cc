e6ce7823167c70dd1707effd1602faac6397eef0
Rewindding <Rewindding@outlook.com>
2020-09-08 11:25:23 +0800

    grammer
    static_cast<>()

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -28,10 +28,10 @@ bool TCPReceiver::segment_received(const TCPSegment &seg) {
     else if(seg.header().syn) {return false;}//duplicate
     //absolute index 数据报文的起始字节编号和终止字节编号，包含fin和syn
     auto seqno_start=unwrap(seg.header().seqno,_isn,_checkpoint);
-    auto seqno_end=seqno_start+max(seg.length_in_sequence_space()-1,static_case<size_t>(0));
+    auto seqno_end=seqno_start+max(seg.length_in_sequence_space()-1,static_cast<size_t>(0));
     auto window_start=unwrap(_ack,_isn,_checkpoint); 
     //window size==0的时候需要发送方持续发送数据，直到窗口size不等于0
-    auto window_end=window_start+max(window_size()-1,static_case<size_t>(0));//if window size==0?
+    auto window_end=window_start+max(window_size()-1,static_cast<size_t>(0));//if window size==0?
     //determine if it's out of range
     bool fall_in_window=(seqno_start<=window_end&&seqno_start>=window_start)||(seqno_end<=window_end&&seqno_end>=window_start);
     if(!fall_in_window) return false;
