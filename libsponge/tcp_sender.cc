c44d3219093d7b0ea53eb25cd7f48060a6c26493
Rewindding <Rewindding@outlook.com>
2020-09-23 20:32:02 +0800

    Update tcp_sender.cc

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -88,6 +88,7 @@ void TCPSender::tick(const size_t ms_since_last_tick) {
     if(duration>=_RTO&&!_outstanding_segs.empty()){//time out
         //assert(!_outstanding_segs.empty());//assertion failed!!! why?
         if(_consecutive_retransmission_time>=TCPConfig::MAX_RETX_ATTEMPTS) return;
+        cout<<"retransmit\n";
         TCPSegment& retran_seg=_outstanding_segs.front();
         _segments_out.push(retran_seg);
         if(_rcv_window_size>0){
