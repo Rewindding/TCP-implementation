2bc7c847eb0793075264a82a17f21e7a4ca843d7
Rewindding <Rewindding@outlook.com>
2020-09-23 20:10:15 +0800

    Update tcp_connection.cc

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -111,9 +111,6 @@ void TCPConnection::connect() {
 }
 
 void TCPConnection::send_segment(){
-    if(_sender.consecutive_retransmissions()>=_cfg.MAX_RETX_ATTEMPTS){
-        return;
-    }
     auto& sender_seg_que=_sender.segments_out();
     while(!sender_seg_que.empty()){
         //put ack at the same time, TCPSender it self did't know TCPReceiver's ackno
