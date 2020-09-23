bd4bb8535cab73c40d739e8d9e2e9145251a4bf0
Rewindding <Rewindding@outlook.com>
2020-09-23 20:59:41 +0800

    Update fsm_retx.cc

--- tests/fsm_retx.cc
+++ tests/fsm_retx.cc
@@ -49,7 +49,7 @@ int main() {
                 test_1.execute(Tick((cfg.rt_timeout << i) - i));  // exponentially increasing delay length
                 test_1.execute(ExpectNoSegment{}, "test 1 failed: re-tx too fast after timeout");
                 test_1.execute(Tick(i));
-                check_segment(test_1, data, i==TCPConfig::MAX_RETX_ATTEMPTS-1, __LINE__);//最后一次循环过不了什么鬼？？？
+                check_segment(test_1, data, i==7, __LINE__);//最后一次循环过不了什么鬼？？？
             }
 
             test_1.execute(ExpectState{State::ESTABLISHED});
